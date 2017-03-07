b565259e3b3692325ab17c6552ad2456815e7492
cchanna <cerisa.accounts@strangerelics.com>
2017-03-06 22:06:20 -0800

    Add previously ignored files

---
+++ code/debug.bat
@@ -0,0 +1,2 @@
+@echo off
+devenv ..\..\build\alpha_cube_win.exe
---
+++ misc/shell.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set path=C:\projects\SDL-tetris\misc\;%path%
+cd C:\projects\SDL-tetris\code\
